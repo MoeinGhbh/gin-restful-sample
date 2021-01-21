30883d667e9b5e30e77c3f63cc020dcbf3e59338
moein ghobbeh <m.ghobbeh@vriday.net>
2021-01-21 16:20:12 +0100

    define restful api with gin framwork

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +13,51 @@ func HomePage(c *gin.Context) {
 	})
 }
 
-func main() {
-	fmt.Println("asdfasfdasd")
+func about(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"data": "dfghdgfdhfg",
+	})
+}
 
+func HomePostPage(c *gin.Context) {
+	body := c.Request.Body
+	value, err := ioutil.ReadAll(body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	c.JSON(200, gin.H{
+		"messenger": string(value),
+	})
+}
+
+func QueryStrings(c *gin.Context) {
+	name := c.Query("name")
+	age := c.Query("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
+}
+
+func pathParameters(c *gin.Context) {
+	name := c.Param("name")
+	age := c.Param("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
+
+}
+
+func main() {
 	r := gin.Default()
 	r.GET("/", HomePage)
+	r.GET("/about", about)
+	r.POST("/", HomePostPage)
+	r.GET("/query", QueryStrings)             // /query?name=moein&age=39
+	r.GET("/path/:name/:age", pathParameters) // /path/moein/54/
+
 	r.Run()
 }
